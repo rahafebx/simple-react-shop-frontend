53823d49ea8ca1d21a3e35ea5e0f284baae8bc9d
rahafebx <rahafebx@gmail.com>
2026-06-29 18:21:39 +0300

    feat: enhance Navbar component with toggle functionality and update MainLayout to manage open state

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,26 @@
 import { Link } from "react-router-dom";
 
-export default function Navbar() {
+export default function Navbar({ isOpen = false }) {
   return (
-    <nav className="bg-neutral-100 dark:bg-neutral-950 text-gray-900 dark:text-gray-50 text-sm">
-      <ul className="flex space-x-4">
+    <nav
+      className={`bg-neutral-100 dark:bg-neutral-950 text-gray-900 dark:text-gray-50 text-sm absolute md:static opacity-0 md:opacity-100 top-16 left-0 right-0 
+        max-h-0 overflow-hidden md:overflow-visible transition-max-h transition-opacity duration-200 ease-in-out
+        ${isOpen ? "max-h-lvh opacity-100" : "max-h-0 opacity-0 md:max-h-none"}`}
+    >
+      <ul className="flex flex-col md:flex-row py-6 md:py-0 ">
         <li>
-          <Link to="/" className="hover:text-blue-500 transition-colors duration-200">
+          <Link
+            to="/"
+            className="hover:text-blue-500 transition-colors duration-200 block px-4 py-2 md:py-4"
+          >
             Home
           </Link>
         </li>
         <li>
-          <Link to="/cart" className="hover:text-blue-500 transition-colors duration-200">
+          <Link
+            to="/cart"
+            className="hover:text-blue-500 transition-colors duration-200 block px-4 py-2 md:py-4"
+          >
             Cart
           </Link>
         </li>
--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
-import { ShoppingCart, LogOut} from 'lucide-react'
+import { ShoppingCart, LogOut, Menu, X } from 'lucide-react'
+import { useState } from "react";
 import ThemeToggle from "../components/ThemeToggle";
 import Navbar from "../components/Navbar";
 import { Link } from "react-router-dom";
@@ -12,14 +13,15 @@ export default function MainLayout() {
   const navigation = useNavigate();
   const {user, logout} = useAuth();
   const { totalItems } = useCart();
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
     <div className="min-h-screen flex flex-col">
       <header className="fixed top-0 left-0 right-0 bg-neutral-100 dark:bg-neutral-950 z-50">
         <Container className="flex items-center justify-between py-4 text-gray-900 dark:text-gray-50">
-          <h1 className="text-xl font-bold">REBEX Shop</h1>
-          <Navbar />
-          <div className="controls flex items-center space-x-3">
+          <h1 className="text-lg sm:text-xl font-bold">REBEX <span className="text-xs text-primary-500 dark:text-primary-400">Shop</span></h1>
+          <Navbar isOpen={isOpen} />
+          <div className="controls flex items-center space-x-3 md:space-x-4">
             <Link to="/cart" className="relative flex items-center justify-center rounded-lg p-2 text-black dark:text-white transition-colors cursor-pointer hover:bg-neutral-200 dark:hover:bg-neutral-900">
               <ShoppingCart size={20} aria-hidden="true" />
               <span className="absolute flex items-center justify-center -top-1 -right-1 w-5 h-5 text-xs bg-primary-500 rounded-full text-white">{totalItems || 0}</span>
@@ -42,6 +44,11 @@ export default function MainLayout() {
               </Link>
             )}
             <ThemeToggle className="hover:bg-neutral-200 dark:hover:bg-neutral-900" />
+            <button className="flex md:hidden items-center justify-center rounded-lg p-2 text-black dark:text-white transition-colors cursor-pointer hover:bg-neutral-200 dark:hover:bg-neutral-900"
+              onClick={() => setIsOpen(!isOpen)}
+            >
+              {isOpen ? (<X size={20} aria-label="close Menu" />) : (<Menu size={20} aria-label="open Menu" />)}
+            </button>
           </div>
         </Container>
       </header>
